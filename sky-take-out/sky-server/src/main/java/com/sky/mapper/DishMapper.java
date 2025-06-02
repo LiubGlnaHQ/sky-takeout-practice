7eecfbddc062713c0d9f78b411c1fd89447a12e1
Liu_2004 <liu_xiangxue@163.com>
2025-06-02 15:16:06 +0800

    feat: add update dish feature

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,6 +10,8 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface DishMapper {
 
@@ -46,4 +48,17 @@ public interface DishMapper {
      */
     @Delete("delete from dish where id = #{id}")
     void deleteById(Long id);
+    /**
+     * 根据菜品id集合批量删除菜品数据
+     * @param ids
+     */
+    void deleteByIds(List<Long> ids);
+
+    /**
+     * 根据id修改菜品的基本信息
+     * @param dish
+     * @return
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Dish dish);
 }
